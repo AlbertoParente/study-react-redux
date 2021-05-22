387f13813cef87743feadc89d168f3250c272eea
AlbertoParente <albertoparente20@gmail.com>
2021-05-22 17:27:35 -0300

    Creating Interval component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg'
 import './app.css'
 import Card from './components/Card'
+import Interval from './components/Interval'
 
 function App() {
   return (
     <div className="App">
       <h1>Activity React-Redux (Simple)</h1>
       <div className="line">
-        <Card title="Card 1" red>X</Card>
-        <Card title="Card 2" green>X</Card>
+        <Interval />
       </div>
       <div className="line">
+        <Card title="Card 2" green>X</Card>
         <Card title="Card 3" blue>Y</Card>
         <Card title="Card 4" purple>Y</Card>
       </div>
@@ -18,4 +19,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
---
+++ src/components/Interval.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './interval.css'
+import Card from './Card'
+
+export default props => {
+    return (
+        <Card title="Interval of Number" red>
+            <div className="Interval">
+                <span>
+                    <strong>Min:</strong>
+                    <input type="number" value={0} readOnly />
+                </span>
+                <span>
+                    <strong>Max:</strong>
+                    <input type="number" value={10} readOnly />
+                </span>
+            </div>
+                
+        </Card>
+    )
+}
